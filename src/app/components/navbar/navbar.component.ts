5efceeb67f9377005f1232f9c14d15b5db627cfc
lauma.alves <laumasantos@gmail.com>
2023-04-26 13:23:08 -0300

    ajuste navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,14 +8,23 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
   isActive = true
+  currentPath?: string;
   hoje!: Date;
-  constructor(private router : Router){}
+  logoUrl!: string;
+  showButton!:boolean;
+  constructor(
+    private router : Router,
+    private route:ActivatedRoute
+  ){}
 
   trogleActive(){
     this.isActive = !this.isActive;
   }
   ngOnInit(): void {
     this.hoje = new Date();
+    this.currentPath = this.route.snapshot.url[0].path;
+    this.logoUrl = this.currentPath === 'main' ? 'assets/logo-navbar.svg' : 'assets/logo.svg';
+    this.showButton = this.currentPath !== 'main'
   }
 
   sair(){
